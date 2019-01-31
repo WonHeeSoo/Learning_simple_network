e04c3528d0d93d3610b1a63729f0e19e7a8323e3
wonhs717 <wonhs717@gmail.com>
2019-02-01 02:25:40 +0900

    Design chat ui

--- Unity Projects/NetworkProject/Assets/02Scripts/NetworkServer.cs
+++ Unity Projects/NetworkProject/Assets/02Scripts/NetworkServer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 //using System.Text;
 
 public class NetworkServer : MonoBehaviour
@@ -14,7 +15,9 @@ public class NetworkServer : MonoBehaviour
 
 	byte[] bytes = null;
 	string data = null;
-	
+
+	Thread th = null;
+
 	private void Awake()
 	{
 		localAddr = IPAddress.Parse("127.0.0.1");
@@ -24,6 +27,11 @@ public class NetworkServer : MonoBehaviour
 		bytes = new byte[256];
 		data = null;
 		Debug.Log("Awake");
+
+		th = new Thread(new ThreadStart(StartServerFunc))
+		{
+			IsBackground = true
+		};
 	}
 
 	private void Start()
@@ -36,9 +44,48 @@ public class NetworkServer : MonoBehaviour
 	{
 		if (Input.GetButtonUp("Fire1"))
 		{
-			StartCoroutine("StartServer");
+			th.Start();
+			//StartCoroutine("StartServer");
 		}
 
+	}
+
+	void StartServerFunc()
+	{
+		Debug.Log("시작");
+
+		Debug.Log("반복문 접속");
+		
+
+		while (true)
+		{
+			Debug.Log("Waiting for a connection...");
+			TcpClient client = server.AcceptTcpClient();
+			Debug.Log("Connectied!");
+
+			data = null;
+
+			NetworkStream stream = client.GetStream();
+
+			int i;
+
+			while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+			{
+				data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+				Debug.Log("Received : " + data);
+
+				data = data.ToUpper();
+
+				byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+				stream.Write(msg, 0, msg.Length);
+				Debug.Log("Sent : " + data);
+				Debug.Log("내부한바퀴 돔");
+			}
+
+			client.Close();
+			Debug.Log("한바퀴 돔");
+		}
 	}
 
 	IEnumerator StartServer()
