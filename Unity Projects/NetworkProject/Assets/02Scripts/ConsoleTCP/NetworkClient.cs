35176c46aaa899a0b7876f5e36cad893518469fe
wonhs717 <wonhs717@gmail.com>
2019-02-05 15:22:27 +0900

    Folder classification

--- Unity Projects/NetworkProject/Assets/02Scripts/ConsoleTCP/NetworkClient.cs
+++ Unity Projects/NetworkProject/Assets/02Scripts/ConsoleTCP/NetworkClient.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Net.Sockets;
 using System.Net;
 using System.IO;
+using System;
 
 public class NetworkClient : MonoBehaviour
 {
@@ -20,44 +21,53 @@ public class NetworkClient : MonoBehaviour
 	{
 		if (Input.GetKeyUp(KeyCode.A))
 		{
-			StartCoroutine(Connect(message1));
+			Connect(message1);
 		}
 		if (Input.GetKeyUp(KeyCode.S))
 		{
-			StartCoroutine(Connect(message2));
+			Connect(message2);
 		}
 		if (Input.GetKeyUp(KeyCode.D))
 		{
-			StartCoroutine(Connect(message3));
+			Connect(message3);
 		}
 		if (Input.GetKeyUp(KeyCode.F))
 		{
-			StartCoroutine(Connect(message4));
+			Connect(message4);
 		}
 		if (Input.GetKeyUp(KeyCode.G))
 		{
-			StartCoroutine(Connect(message5));
+			Connect(message5);
 		}
 	}
 
-	IEnumerator Connect(string message)
+	void Connect(string message)
 	{
-		client = new TcpClient("127.0.0.1", port);
-		//data = System.Text.Encoding.ASCII.GetBytes(message);
-		data = System.Text.Encoding.UTF8.GetBytes(message);
-		NetworkStream stream = client.GetStream();
-		stream.Write(data, 0, data.Length);
-		data = new byte[256];
-		string responseData = string.Empty;
+		try
+		{
+			client = new TcpClient("127.0.0.1", port);
+			//data = System.Text.Encoding.ASCII.GetBytes(message);
+			data = System.Text.Encoding.UTF8.GetBytes(message);
+			NetworkStream stream = client.GetStream();
+			stream.Write(data, 0, data.Length);
+			data = new byte[256];
+			string responseData = string.Empty;
 
-		int bytes = stream.Read(data, 0, data.Length);
-		//responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-		responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
-		Debug.Log("Received: " + responseData);
+			int bytes = stream.Read(data, 0, data.Length);
+			//responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+			responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+			Debug.Log("Received: " + responseData);
 
-		stream.Close();
-		client.Close();
-
-		yield return null;
+			stream.Close();
+			client.Close();
+		}
+		catch (ArgumentNullException e)
+		{
+			Debug.Log("ArgumentNullException: " + e);
+		}
+		catch (SocketException e)
+		{
+			Debug.Log("SocketException: " + e);
+		}
 	}
 }
