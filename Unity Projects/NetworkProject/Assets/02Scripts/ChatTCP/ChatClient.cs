07a5289dcd2abce498b031bd23c0e2474ec38a9b
WHS <wonhs717@gmail.com>
2019-02-16 21:00:52 +0900

    Edit Chat server, client script

--- Unity Projects/NetworkProject/Assets/02Scripts/ChatTCP/ChatClient.cs
+++ Unity Projects/NetworkProject/Assets/02Scripts/ChatTCP/ChatClient.cs
@@ -5,69 +5,89 @@ using System.Net.Sockets;
 using System.Net;
 using System.IO;
 using System;
+using UnityEngine.UI;
+using System.Threading;
 
 public class ChatClient : MonoBehaviour
 {
-	int port = 13000;
-	TcpClient client = null;
-	string message1 = "안녕하세요 반갑습니다.";
-	string message2 = "Hi, Hello abcdefghijklmnopqrstuvwxyz";
-	string message3 = "바자다가사 마나아라하 카타차파";
-	string message4 = "뵤벼뱌배베보버바비뷰부브";
-	string message5 = ",./;'[]<>?:%^&*()_+-=~`!@#$%^&";
-	byte[] data;
+    int port = 13000;
+    string ip = "127.0.0.1";
+    NetworkStream stream = null;
+    TcpClient client = null;
+    string message1 = "안녕하세요 반갑습니다.";
+    string message2 = "Hi, Hello abcdefghijklmnopqrstuvwxyz";
+    string message3 = "바자다가사 마나아라하 카타차파";
+    string message4 = "뵤벼뱌배베보버바비뷰부브";
+    string message5 = ",./;'[]<>?:%^&*()_+-=~`!@#$%^&";
+    byte[] data;
 
-	private void Update()
-	{
-		if (Input.GetKeyUp(KeyCode.A))
-		{
-			Connect(message1);
-		}
-		if (Input.GetKeyUp(KeyCode.S))
-		{
-			Connect(message2);
-		}
-		if (Input.GetKeyUp(KeyCode.D))
-		{
-			Connect(message3);
-		}
-		if (Input.GetKeyUp(KeyCode.F))
-		{
-			Connect(message4);
-		}
-		if (Input.GetKeyUp(KeyCode.G))
-		{
-			Connect(message5);
-		}
-	}
+    public Text textObject;
 
-	void Connect(string message)
-	{
-		try
-		{
-			client = new TcpClient("127.0.0.1", port);
-			//data = System.Text.Encoding.ASCII.GetBytes(message);
-			data = System.Text.Encoding.UTF8.GetBytes(message);
-			NetworkStream stream = client.GetStream();
-			stream.Write(data, 0, data.Length);
-			data = new byte[256];
-			string responseData = string.Empty;
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            textObject.text = SendString(message1);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            textObject.text = SendString(message2);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            textObject.text = SendString(message3);
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            textObject.text = SendString(message4);
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            textObject.text = SendString(message5);
+        }
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            client = new TcpClient(ip, port);
+            stream = client.GetStream();
+        }
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            stream.Close();
+            client.Close();
+        }
+    }
 
-			int bytes = stream.Read(data, 0, data.Length);
-			//responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-			responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
-			Debug.Log("Received: " + responseData);
+    string SendString(string message)
+    {
+        //client = new TcpClient(ip, port);
+        //stream = client.GetStream();
 
-			stream.Close();
-			client.Close();
-		}
-		catch (ArgumentNullException e)
-		{
-			Debug.Log("ArgumentNullException: " + e);
-		}
-		catch (SocketException e)
-		{
-			Debug.Log("SocketException: " + e);
-		}
-	}
+        string responseData = string.Empty;
+        try
+        {
+            //data = System.Text.Encoding.ASCII.GetBytes(message);
+            data = System.Text.Encoding.UTF8.GetBytes(message);
+
+            stream.Write(data, 0, data.Length);
+            data = new byte[256];
+            //string responseData = string.Empty;
+
+            int bytes = stream.Read(data, 0, data.Length);
+            //responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+            Debug.Log("Received: " + responseData);
+
+            //stream.Close();
+            //client.Close();
+        }
+        catch (ArgumentNullException e)
+        {
+            Debug.Log("ArgumentNullException: " + e);
+        }
+        catch (SocketException e)
+        {
+            Debug.Log("SocketException: " + e);
+        }
+        return responseData;
+    }
 }
