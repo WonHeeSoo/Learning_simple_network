a77de6f467a9f497799d05abeebe9800b50c0d2e
WHS <wonhs717@gmail.com>
2019-02-18 22:50:11 +0900

    Edit ReadWriteFunc function

--- Unity Projects/NetworkProject/Assets/02Scripts/ChatTCP/ChatServer.cs
+++ Unity Projects/NetworkProject/Assets/02Scripts/ChatTCP/ChatServer.cs
@@ -44,20 +44,23 @@ public class ChatServer : MonoBehaviour
 
     void ReadWriteFunc()
     {
-        data = null;
-
-        int i;
-
-        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+        lock(streamList)
         {
-            //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-            data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
-            Debug.Log("Received : " + data);
+            data = null;
 
-            //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-            byte[] msg = System.Text.Encoding.UTF8.GetBytes(data);
-            stream.Write(msg, 0, msg.Length);
-            Debug.Log("Sent : " + data);
+            int i;
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                Debug.Log("Received : " + data);
+
+                //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                byte[] msg = System.Text.Encoding.UTF8.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+                Debug.Log("Sent : " + data);
+            }
         }
     }
 
